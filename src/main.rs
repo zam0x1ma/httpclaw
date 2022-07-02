954da7f326b3ef8d7579308f23ac33386e4ceebe
zam0x1ma <56079201+zam0x1ma@users.noreply.github.com>
2022-07-02 01:58:11 -0300

    Add flags support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
+use clap::Parser;
 use reqwest::Client;
 use reqwest::Method;
 use reqwest::header;
 use std::io::prelude::*;
 use std::io;
 
+/// Take a list of domains and probe for working http and https servers
+#[derive(Parser, Debug)]
+#[clap(about)]
+struct Args {
+    /// Set the concurrency level (split equally between HTTPS and HTTP requests)
+    #[clap(short, long, value_parser, default_value_t = 20)]
+    concurrency: u32,
+
+    /// Add additional probe (protocol:port)
+    #[clap(short, long, value_parser)]
+    probes: Vec<String>,
+
+    /// Skip the default probes (http:80 and https:443)
+    #[clap(short, long, value_parser, default_value_t = false)]
+    skip_default: bool,
+
+    /// Timeout (miliseconds)
+    #[clap(short, long, value_parser, default_value_t = 10000)]
+    timeout: u32,
+
+    /// Only try plain HTTP if HTTPS fails
+    #[clap(long, value_parser, default_value_t = false)]
+    prefer_https: bool,
+
+    /// HTTP method to use
+    #[clap(short, long, value_parser, default_value = "GET")]
+    method: String,
+}
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+
     let stdin = io::stdin();
     let client = Client::new();
     for line in stdin.lock().lines() {
